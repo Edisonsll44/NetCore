0a15269d98a90d81d1fd67e707e781faad7c06b9
Eddy <edison.sanchez@ppm.com.ec>
2025-03-06 23:57:22 -0500

    add initial database migration and project configuration files

---
+++ ClinicMedicalAppointments/ClinicMedicalAppointments.Infraestructure/Migrations/20250307034128_InitialCreate.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicMedicalAppointments.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "especialidad",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TipoEspecialidad = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_especialidad", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "propietario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NombrePropietario = table.Column<string>(type: "text", nullable: false),
+                    ApellidoPropietario = table.Column<string>(type: "text", nullable: false),
+                    Direccion = table.Column<string>(type: "text", nullable: false),
+                    Telefono = table.Column<string>(type: "text", nullable: false),
+                    Ciudad = table.Column<string>(type: "text", nullable: false),
+                    Correo = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_propietario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "raza",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_raza", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "receta_medica",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Prescripcion = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_receta_medica", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rol",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Descripcion = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rol", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "mascota",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Sexo = table.Column<string>(type: "text", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Edad = table.Column<string>(type: "text", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: false),
+                    RazaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PropietarioId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mascota", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_mascota_propietario_PropietarioId",
+                        column: x => x.PropietarioId,
+                        principalTable: "propietario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_mascota_raza_RazaId",
+                        column: x => x.RazaId,
+                        principalTable: "raza",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NombreUsuario = table.Column<string>(type: "text", nullable: false),
+                    Correo = table.Column<string>(type: "text", nullable: false),
+                    Contrasena = table.Column<string>(type: "text", nullable: false),
+                    RolId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_usuario_rol_RolId",
+                        column: x => x.RolId,
+                        principalTable: "rol",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "medico_veterinario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Cedula = table.Column<string>(type: "text", nullable: false),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Apellido = table.Column<string>(type: "text", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Correo = table.Column<string>(type: "text", nullable: false),
+                    Celular = table.Column<string>(type: "text", nullable: false),
+                    EspecialidadId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_medico_veterinario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_medico_veterinario_especialidad_EspecialidadId",
+                        column: x => x.EspecialidadId,
+                        principalTable: "especialidad",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_medico_veterinario_usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "historia_clinica",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FechaAdmision = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MedicoVeterinarioId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MascotaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_historia_clinica", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_historia_clinica_mascota_MascotaId",
+                        column: x => x.MascotaId,
+                        principalTable: "mascota",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_historia_clinica_medico_veterinario_MedicoVeterinarioId",
+                        column: x => x.MedicoVeterinarioId,
+                        principalTable: "medico_veterinario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "consulta_medica",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MotivoConsulta = table.Column<string>(type: "text", nullable: false),
+                    Vacunas = table.Column<string>(type: "text", nullable: false),
+                    Desparasitacion = table.Column<string>(type: "text", nullable: false),
+                    FechaDesparasitacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EstadoReproductivo = table.Column<string>(type: "text", nullable: false),
+                    Procedencia = table.Column<string>(type: "text", nullable: false),
+                    Diagnostico = table.Column<string>(type: "text", nullable: false),
+                    Pronostico = table.Column<string>(type: "text", nullable: false),
+                    Observaciones = table.Column<string>(type: "text", nullable: false),
+                    RecetaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RecetaMedicaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HistoriaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HistoriaClinicaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Estado = table.Column<bool>(type: "boolean", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_consulta_medica", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_consulta_medica_historia_clinica_HistoriaClinicaId",
+                        column: x => x.HistoriaClinicaId,
+                        principalTable: "historia_clinica",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_consulta_medica_receta_medica_RecetaMedicaId",
+                        column: x => x.RecetaMedicaId,
+                        principalTable: "receta_medica",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_consulta_medica_HistoriaClinicaId",
+                table: "consulta_medica",
+                column: "HistoriaClinicaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_consulta_medica_RecetaMedicaId",
+                table: "consulta_medica",
+                column: "RecetaMedicaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_historia_clinica_MascotaId",
+                table: "historia_clinica",
+                column: "MascotaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_historia_clinica_MedicoVeterinarioId",
+                table: "historia_clinica",
+                column: "MedicoVeterinarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mascota_PropietarioId",
+                table: "mascota",
+                column: "PropietarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mascota_RazaId",
+                table: "mascota",
+                column: "RazaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_medico_veterinario_EspecialidadId",
+                table: "medico_veterinario",
+                column: "EspecialidadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_medico_veterinario_UsuarioId",
+                table: "medico_veterinario",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuario_RolId",
+                table: "usuario",
+                column: "RolId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "consulta_medica");
+
+            migrationBuilder.DropTable(
+                name: "historia_clinica");
+
+            migrationBuilder.DropTable(
+                name: "receta_medica");
+
+            migrationBuilder.DropTable(
+                name: "mascota");
+
+            migrationBuilder.DropTable(
+                name: "medico_veterinario");
+
+            migrationBuilder.DropTable(
+                name: "propietario");
+
+            migrationBuilder.DropTable(
+                name: "raza");
+
+            migrationBuilder.DropTable(
+                name: "especialidad");
+
+            migrationBuilder.DropTable(
+                name: "usuario");
+
+            migrationBuilder.DropTable(
+                name: "rol");
+        }
+    }
+}
